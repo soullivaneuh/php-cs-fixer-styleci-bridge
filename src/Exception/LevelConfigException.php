6d288ed1b6d1bdf7e33a2859d402f5ae6b84e118
Sullivan SENECHAL <sullivan@nexylan.com>
2015-09-21 14:21:54 +0200

    PHP CS Fixer bridge

---
+++ src/Exception/LevelConfigException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SLLH\StyleCIBridge\Exception;
+
+/**
+ * Exception for level/preset parsing.
+ *
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class LevelConfigException extends AbstractConfigException
+{
+}
